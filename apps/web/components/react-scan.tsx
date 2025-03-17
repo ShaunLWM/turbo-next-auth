0003250925a516bf0cd0857e81c925025bb19ab9
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2025-03-17 09:31:25 +0800

    feat: add react-scan

---
+++ apps/web/components/react-scan.tsx
@@ -0,0 +1,17 @@
+// path/to/ReactScanComponent
+
+"use client";
+// react-scan must be imported before react
+import { scan } from "react-scan";
+
+import { type JSX, useEffect } from "react";
+
+export function ReactScan(): JSX.Element {
+	useEffect(() => {
+		scan({
+			enabled: process.env.NODE_ENV !== "production",
+		});
+	}, []);
+
+	return <></>;
+}
