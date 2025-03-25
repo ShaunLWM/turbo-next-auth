cef44ea230316851a1c45715ae129b9a60b21e0e
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2025-03-26 06:20:40 +0800

    chore: add simple metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { ReactScan } from "@/components/react-scan";
+import type { Metadata } from "next";
 
 const fontSans = Geist({
 	subsets: ["latin"],
@@ -13,6 +14,57 @@ const fontMono = Geist_Mono({
 	subsets: ["latin"],
 	variable: "--font-mono",
 });
+
+export const metadata: Metadata = {
+	title: "",
+	description: "",
+	keywords: [],
+	authors: [],
+	creator: "",
+	publisher: "",
+	formatDetection: {
+		email: false,
+		telephone: false,
+	},
+	metadataBase: new URL(""),
+	alternates: {
+		canonical: "",
+	},
+	openGraph: {
+		title: "",
+		description: "",
+		url: "",
+		siteName: "",
+		images: [
+			{
+				url: "",
+				width: 0,
+				height: 0,
+				alt: "",
+			},
+		],
+		locale: "",
+		type: "website",
+	},
+	twitter: {
+		card: "app",
+		title: "",
+		description: "",
+		images: [],
+	},
+	robots: {
+		index: false,
+		follow: false,
+		googleBot: {
+			index: false,
+			follow: false,
+			"max-image-preview": "large",
+			"max-video-preview": -1,
+			"max-snippet": -1,
+		},
+	},
+	category: "",
+};
 
 export default function RootLayout({
 	children,
