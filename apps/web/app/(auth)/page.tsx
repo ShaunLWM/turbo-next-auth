344035688725aa2f610dba02ecef000a7c224ad2
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2025-03-26 09:21:22 +0800

    chore: remove better-auth from web

--- apps/web/app/(auth)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { useSession } from "@/lib/auth-client";
-
-export default function AuthPage() {
-	const { data } = useSession();
-
-	return <div>Welcome to the app {data?.user.email}</div>;
-}
--- apps/web/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export type AuthClient = ReturnType<typeof createAuthClient>;
-
-export const authClient = createAuthClient({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-}) as AuthClient;
-
-export const useSession = () => authClient.useSession();
