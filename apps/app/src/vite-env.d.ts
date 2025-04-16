22d402e075493b49cd7c1bb9e7f9edd1f90b1b8f
ShaunLWM <37434846+ShaunLWM@users.noreply.github.com>
2025-04-16 14:35:46 +0800

    feat(app): add env autocomplete

--- apps/app/src/vite-env.d.ts
+++ apps/app/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly BETTER_AUTH_SECRET: string;
+	readonly VITE_PUBLIC_BASE_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
